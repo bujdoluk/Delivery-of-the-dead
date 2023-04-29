fcd6d15cb526d5f99312273f7bcfed41825af38e
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2023-04-29 13:47:53 +0200

    init

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public RigidBody2D rb;
+    public Weapon Weapon;
+
+    Vector2 moveDirection;
+    Vector2 mousePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
