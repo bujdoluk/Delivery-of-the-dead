681966b99418f0f7907d576b7f2a0e8587055657
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2023-04-29 15:24:49 +0200

    fixed Rigidbody2D

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollistionEnter2D(Collision2D collision) {
+        Destroy(gameObject);
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public RigidBody2D rb;
-    public Weapon Weapon;
-
+    public Weapon weapon;
+    public Rigidbody2D rb;
     Vector2 moveDirection;
     Vector2 mousePosition;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetMouseButtonDown(0)) {
+            weapon.Fire();
+        }
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 }
